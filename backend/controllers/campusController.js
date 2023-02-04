85d79526b0335384365bb79120d74fc4dc40fbf5
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-04 15:29:49 -0800

    Add REST API for campuses

--- backend/controllers/campusController.js
+++ backend/controllers/campusController.js
@@ -1,19 +1,45 @@
 const asyncHandler = require('express-async-handler');
 
+const Campus = require('../models/campusModel');
+
 const getCampuses = asyncHandler(async (req, res) => {
-  res.json({ message: 'Get campuses' });
+  const campuses = await Campus.find({});
+  res.json(campuses);
 });
 
 const createCampus = asyncHandler(async (req, res) => {
-  res.json({ message: 'Add campus' });
+  const campus = await Campus.create(req.body);
+  res.status(200).json(campus);
 });
 
 const updateCampus = asyncHandler(async (req, res) => {
-  res.json({ message: `Update campus ${req.params.id}` });
+  const campus = await Campus.findById(req.params.id);
+
+  if (!campus) {
+    res.status(400);
+    throw new Error('Campus not found.');
+  }
+
+  const updatedCampus = await Campus.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedCampus);
 });
 
 const deleteCampus = asyncHandler(async (req, res) => {
-  res.json({ message: `Delete campus ${req.params.id}` });
+  const campus = await Campus.findById(req.params.id);
+
+  if (!campus) {
+    res.status(400);
+    throw new Error('Campus not found.');
+  }
+
+  const deletedCampus = await campus.remove();
+
+  res.status(200).json(deletedCampus);
 });
 
 module.exports = {
