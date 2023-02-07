785982bb5a7a87f4418924a98e0adada34c16fac
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-07 14:39:45 -0800

    Add container styles

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.2rem',
+        sm: '0rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
