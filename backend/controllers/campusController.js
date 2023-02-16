b1378df049a8b8a0039205ab2bf37db3760fa059
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-15 17:59:10 -0800

    Remove review field from campus

--- backend/controllers/campusController.js
+++ backend/controllers/campusController.js
@@ -7,14 +7,7 @@ const getCampuses = asyncHandler(async (req, res) => {
 });
 
 const getCampus = asyncHandler(async (req, res) => {
-  const campus = await Campus.findById(req.params.id).populate({
-    path: 'reviews',
-    populate: {
-      path: 'author',
-      model: 'User',
-      select: '_id name',
-    },
-  });
+  const campus = await Campus.findById(req.params.id);
   if (!campus) {
     res.status(400);
     throw new Error('Campus not found');
