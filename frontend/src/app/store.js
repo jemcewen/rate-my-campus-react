e1fa4a90b48654db838cdc1427c1c153da159636
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-16 14:10:10 -0800

    Add new reducers

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import campusReducer from '../features/campus/campusSlice';
+import campusesReducer from '../features/campuses/campusesSlice';
+import reviewReducer from '../features/review/reviewSlice';
+import reviewsReducer from '../features/reviews/reviewsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    campuses: campusReducer,
+    campus: campusReducer,
+    campuses: campusesReducer,
+    review: reviewReducer,
+    reviews: reviewsReducer,
   },
 });
