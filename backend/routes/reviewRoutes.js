f5d221009bcbafc37eaa7fcfd48e0041e0040a8d
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-16 14:08:35 -0800

    Add getReviews route

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { createReview } = require('../controllers/reviewController');
+const { getReviews, createReview } = require('../controllers/reviewController');
 const validateReview = require('../middleware/validation/validateReview');
 const protect = require('../middleware/authMiddleware');
 
-router.post('/', protect, validateReview, createReview);
+router.route('/').get(getReviews).post(protect, validateReview, createReview);
 
 module.exports = router;
