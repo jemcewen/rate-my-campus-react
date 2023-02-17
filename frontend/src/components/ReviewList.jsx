0ae66db5291bd8237a8f40d33ced62efe4aea230
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-17 08:03:53 -0800

    Add animation and remove success toast

--- frontend/src/components/ReviewList.jsx
+++ frontend/src/components/ReviewList.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getReviews, reset } from '../features/reviews/reviewsSlice';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 import toast from 'react-hot-toast';
 import Spinner from './Spinner';
 import Review from './Review';
@@ -8,16 +9,15 @@ import Review from './Review';
 const ReviewList = ({ campus }) => {
   const dispatch = useDispatch();
 
-  const { reviews, reviewSubmit, isLoading, isError, isSuccess, message } =
-    useSelector((state) => state.reviews);
+  const [list] = useAutoAnimate();
+
+  const { reviews, isError, isSuccess, message } = useSelector(
+    (state) => state.reviews
+  );
 
   useEffect(() => {
     if (isError) {
       toast.error(message);
-    }
-
-    if (reviewSubmit) {
-      toast.success('Review added!');
     }
 
     dispatch(getReviews(campus));
@@ -25,18 +25,15 @@ const ReviewList = ({ campus }) => {
     return () => {
       dispatch(reset());
     };
-  }, [isError, message, reviewSubmit, dispatch, campus]);
+  }, [isError, message, dispatch, campus]);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
   if (isSuccess) {
     return (
       <div className='flex flex-col gap-8'>
         <h2 className='text-2xl font-semibold underline'>
           {reviews.length} {reviews.length === 1 ? 'review' : 'reviews'}
         </h2>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-8' ref={list}>
           {reviews.map((review) => (
             <Review key={review._id} review={review} />
           ))}
