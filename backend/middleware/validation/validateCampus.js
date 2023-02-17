4ac72656749a6eb562eb30781ffc51b57ec8026c
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-17 15:50:50 -0800

    Add addition validation for input lengths

--- backend/middleware/validation/validateCampus.js
+++ backend/middleware/validation/validateCampus.js
@@ -1,13 +1,18 @@
 const Joi = require('joi');
 
 campusValidation = Joi.object({
-  name: Joi.string()
-    .required()
-    .messages({ 'string.empty': 'Please enter the campus name' }),
-  location: Joi.string()
-    .required()
-    .messages({ 'string.empty': 'Please enter the campus location' }),
-  description: Joi.string().allow('', null),
+  name: Joi.string().required().max(50).messages({
+    'string.empty': 'Please enter the campus name',
+    'string.max': 'Campus name cannot be longer than 50 characters',
+  }),
+  location: Joi.string().required().max(50).messages({
+    'string.empty': 'Please enter the campus location',
+    'string.max': 'Campus location cannot be longer than 50 characters',
+  }),
+  description: Joi.string().required().max(450).messages({
+    'string.empty': 'Please enter the campus description',
+    'string.max': 'Campus description cannot be longer than 450 characters',
+  }),
 });
 
 const validateCampus = (req, res, next) => {
