e1549643b3f4a47a7bcd605a3c76be7da1e7a4e4
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-16 17:38:37 -0800

    Add logic to check if review was submitted

--- frontend/src/components/ReviewList.jsx
+++ frontend/src/components/ReviewList.jsx
@@ -1,34 +1,42 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getReviews, reset } from '../features/reviews/reviewsSlice';
+import toast from 'react-hot-toast';
 import Spinner from './Spinner';
 import Review from './Review';
 
 const ReviewList = ({ campus }) => {
   const dispatch = useDispatch();
 
-  const { reviews, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.reviews
-  );
+  const { reviews, reviewSubmit, isLoading, isError, isSuccess, message } =
+    useSelector((state) => state.reviews);
 
   useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (reviewSubmit) {
+      toast.success('Review added!');
+    }
+
     dispatch(getReviews(campus));
 
     return () => {
       dispatch(reset());
     };
-  }, []);
+  }, [isError, message, reviewSubmit, dispatch, campus]);
 
   if (isLoading) {
     return <Spinner />;
   }
   if (isSuccess) {
     return (
-      <div>
+      <div className='flex flex-col gap-8'>
+        <h2 className='text-2xl font-semibold underline'>
+          {reviews.length} {reviews.length === 1 ? 'review' : 'reviews'}
+        </h2>
         <div className='flex flex-col gap-8'>
-          <h2 className='text-2xl font-semibold underline'>
-            {reviews.length} {reviews.length === 1 ? 'review' : 'reviews'}
-          </h2>
           {reviews.map((review) => (
             <Review key={review._id} review={review} />
           ))}
