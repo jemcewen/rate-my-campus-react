a37c0c447cd0839a50ec55604768d38e94452318
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-16 17:36:08 -0800

    Remove review reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import campusReducer from '../features/campus/campusSlice';
 import campusesReducer from '../features/campuses/campusesSlice';
-import reviewReducer from '../features/review/reviewSlice';
 import reviewsReducer from '../features/reviews/reviewsSlice';
 
 export const store = configureStore({
@@ -10,7 +9,6 @@ export const store = configureStore({
     auth: authReducer,
     campus: campusReducer,
     campuses: campusesReducer,
-    review: reviewReducer,
     reviews: reviewsReducer,
   },
 });
