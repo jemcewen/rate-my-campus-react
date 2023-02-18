426e93355a6fb067c00fcb3ce53f297792b73d05
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-17 16:13:57 -0800

    Add maximum review length

--- backend/middleware/validation/validateReview.js
+++ backend/middleware/validation/validateReview.js
@@ -1,9 +1,10 @@
 const Joi = require('joi');
 
 reviewValidation = Joi.object({
-  body: Joi.string()
-    .required()
-    .messages({ 'string.empty': 'Please enter a review' }),
+  body: Joi.string().required().max(1000).messages({
+    'string.empty': 'Please enter a review',
+    'string.max': 'Review body cannot be longer than 1000 characters',
+  }),
   rating: Joi.number().min(0).max(5).required().messages({
     'number.base': 'Please enter a rating',
     'number.min': 'Rating cannot be less than 0',
