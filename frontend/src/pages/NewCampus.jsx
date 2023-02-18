574648884b8382b59ac0012ced07f8a95755536c
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-17 18:21:48 -0800

    Add image preview

--- frontend/src/pages/NewCampus.jsx
+++ frontend/src/pages/NewCampus.jsx
@@ -135,7 +135,7 @@ const CampusForm = () => {
               Upload images
             </label>
             <div className='flex items-center justify-center w-full'>
-              <label className='flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100'>
+              <label className='flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white  hover:bg-gray-100 shadow'>
                 <div className='flex flex-col items-center justify-center pt-5 pb-6'>
                   <svg
                     aria-hidden='true'
@@ -170,6 +170,15 @@ const CampusForm = () => {
                   onChange={handleChange}
                 />
               </label>
+            </div>
+            <div className='flex flex-wrap h-full mt-4 '>
+              {images &&
+                [...images].map((file) => (
+                  <img
+                    src={URL.createObjectURL(file)}
+                    className='h-[100px] mr-3 mb-3 opacity-80 rounded shadow'
+                  />
+                ))}
             </div>
           </div>
           <div className='flex justify-end'>
