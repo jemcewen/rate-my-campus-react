9a27cab48a99d45b26d0cd4c5a8b4878f491399a
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-21 11:42:45 -0800

    Add api endpoint

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
+const API_ENDPOINT = import.meta.env.VITE_API_URL;
+
 // Register user
 const register = async (userData) => {
-  const response = await axios.post('/api/users/register', userData);
+  const response = await axios.post(
+    `${API_ENDPOINT}/api/users/register`,
+    userData
+  );
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
