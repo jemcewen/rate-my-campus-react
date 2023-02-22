7d5a59fd14b7287042827a008b276a168d622794
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-22 11:40:52 -0800

    Populate reviews and review authors

--- backend/controllers/campusController.js
+++ backend/controllers/campusController.js
@@ -11,7 +11,10 @@ const getCampuses = asyncHandler(async (req, res) => {
 });
 
 const getCampus = asyncHandler(async (req, res) => {
-  const campus = await Campus.findById(req.params.id);
+  const campus = await Campus.findById(req.params.id).populate({
+    path: 'reviews',
+    populate: 'author',
+  });
   if (!campus) {
     res.status(400);
     throw new Error('Campus not found');
