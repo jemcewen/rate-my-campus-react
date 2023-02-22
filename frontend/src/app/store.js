40843361cf7033b11ba106dd2fb9e37c2645b8b6
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-22 11:41:18 -0800

    Remove reviews slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import campusReducer from '../features/campus/campusSlice';
 import campusesReducer from '../features/campuses/campusesSlice';
-import reviewsReducer from '../features/reviews/reviewsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     campus: campusReducer,
     campuses: campusesReducer,
-    reviews: reviewsReducer,
   },
 });
