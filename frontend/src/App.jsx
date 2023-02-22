22c9555bee71a9417b5dfcb5be26fa7dbe781f2f
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-22 09:23:56 -0800

    Add ScrollToTop component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import ScrollToTop from './components/ScrollToTop';
 import Nav from './components/Nav';
 import Explore from './pages/Explore';
 import Register from './pages/Register';
@@ -12,6 +13,7 @@ const App = () => {
     <div className='bg-gray-100 min-h-screen'>
       <Toaster />
       <Router>
+        <ScrollToTop />
         <Nav />
         <Routes>
           <Route path='/' element={<Explore />} />
