777bedfbe88efa8541fdd2687c2f6dfd95e1328c
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-21 17:27:54 -0800

    Change 'build' to 'dist'

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.use('/api/campuses', campusRoutes);
 app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
     )
   );
 }
